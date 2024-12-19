459c2093e37bb46d7abcec7599cbc0ae7e917a4e
Henk S <hsiebelink73@gmail.com>
2024-12-19 19:54:56 +0100

    comment aanpassen

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -3,13 +3,12 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class TaskTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * Tasks testen
      */
 
     use RefreshDatabase;
