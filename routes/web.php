27b1c9182d5375c121572793cf14df9981b6d900
Henk S <hsiebelink73@gmail.com>
2024-12-20 13:47:09 +0100

    Redirect naar todo-app

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('tasks');
 });
 
 Route::resource('tasks', TaskController::class);
